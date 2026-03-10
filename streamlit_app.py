30ae23ce1bbd20f1da80e611ee583922c39a63b2
cem evecen <cemevecen@MacBookPro.home>
2026-03-10 08:54:54 +0300

    feat: hide extended analysis prompt if remaining comments < 500

--- streamlit_app.py
+++ streamlit_app.py
@@ -1487,7 +1487,7 @@ if "bulk_results" in st.session_state:
         analyzed_count = len(df)
         remaining_pool = all_pool[analyzed_count:]
         
-        if remaining_pool:
+        if len(remaining_pool) >= 500:
             st.markdown('<div class="fancy-divider"></div>', unsafe_allow_html=True)
             with st.container(border=True):
                 st.markdown(f"""
